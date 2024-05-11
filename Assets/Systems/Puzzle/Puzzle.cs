b74598dae95ed3cf41ad83bfdf42992435261c1a
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-05-11 16:58:45 -0300

    Añadida clase Puzzle

---
+++ Assets/Systems/Puzzle/Puzzle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PuzzleSystem
+{
+    public abstract class Puzzle : MonoBehaviour
+    {
+        private bool _completed = false;
+        public bool Completed
+        {
+            get { return _completed; }
+            protected set
+            {
+                if (_completed != value)
+                {
+                    _completed = value;
+                    PuzzleStateUpdated.Invoke();
+                }
+            }
+        }
+
+        public UnityAction PuzzleStateUpdated;
+
+    }
+}

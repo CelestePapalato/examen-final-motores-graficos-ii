97a3a37068baf02384b78f30dbcaff33416217d7
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-07-14 22:26:08 -0300

    Añadida barra de vida

---
+++ Assets/Scripts/UI/Healthbar.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Health healthComponent;
+    private Slider healthbarSlider;
+
+    private void Awake()
+    {
+        healthbarSlider = GetComponent<Slider>();
+        if (!healthbarSlider || !healthComponent) { gameObject.SetActive(false); }
+    }
+
+    private void OnEnable()
+    {
+        if (healthComponent)
+        {
+            healthComponent.onHealthUpdate += UpdateSlider;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (healthComponent)
+        {
+            healthComponent.onHealthUpdate -= UpdateSlider;
+        }
+    }
+
+    public void UpdateSlider(int value, int maxValue)
+    {
+        float slider_value = (float) value / maxValue;
+        healthbarSlider.value = slider_value;
+    }
+}

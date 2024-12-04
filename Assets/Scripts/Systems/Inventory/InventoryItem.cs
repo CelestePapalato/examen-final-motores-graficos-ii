6976826344873f3fd3096a0d86d42ba3cba1b06f
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-12-04 12:46:05 -0300

    Añadido ejemplo de Game Object para items de inventario. Ajustes en el código.

--- Assets/Scripts/Systems/Inventory/InventoryItem.cs
+++ Assets/Scripts/Systems/Inventory/InventoryItem.cs
@@ -6,11 +6,13 @@ namespace InventorySystem
     public class InventoryItem : Item
     {
         [SerializeField] private ItemSO _itemData;
+        [SerializeField] private GameObject destroyOnPick;
         public override void Grab()
         {
             base.Grab();
             Inventory.AddItem(_itemData);
-            Destroy(gameObject);
+            if (!destroyOnPick) { destroyOnPick = gameObject; }
+            Destroy(destroyOnPick);
         }
     }
 }
